babbb46e45d0e065f23ddbcc388ba5b2e6a9f895
Sangho Na <microamp@protonmail.com>
2018-01-07 15:33:42 +1300

    Replace SteelBlue3 with LightSkyBlue3

--- config.h
+++ config.h
@@ -17,8 +17,8 @@ enum {
 };
 
 static Color colors[] = {
-	[DEFAULT] = { .fg = -1,         .bg = -1, .fg256 = -1, .bg256 = -1, },
-	[BLUE]    = { .fg = COLOR_BLUE, .bg = -1, .fg256 = 68, .bg256 = -1, },
+	[DEFAULT] = { .fg = -1,         .bg = -1, .fg256 = -1,  .bg256 = -1, },
+	[BLUE]    = { .fg = COLOR_BLUE, .bg = -1, .fg256 = 110, .bg256 = -1, },
 };
 
 #define COLOR(c)        COLOR_PAIR(colors[c].pair)
